7142c3c1e2a81417a852b539b5b266f69cee3295
Olya_Polyanskaya <polyanskaya3000@gmail.com>
2022-01-10 00:16:40 +0700

    feat: finish task Reverse Int'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+  let result = '';
+  let x = Math.abs(n);
+  if (x < 10) {
+      return x;
+  }
+    while (x !== 0) {
+        result = `${result}${x % 10}`;
+        x = Math.floor(x / 10);
+    }
+    return Number(result);
 }
